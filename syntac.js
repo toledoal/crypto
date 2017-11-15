5e87a463a00a6e8b91919859ddeb596df405de77
toledoal <toledoal@users.noreply.github.com>
2017-11-14 19:11:17 -0600

    first commit

---
+++ syntac.js
@@ -0,0 +1,11 @@
+'use strict'
+var prices = [12, 20, 18];
+var newPriceArray = [...prices];
+console.log(newPriceArray);
+
+var lang = 'en-US';
+var esVal = 121;
+
+var english = {lang, esVal};
+
+console.log(english);
